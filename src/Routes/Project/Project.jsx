c31667637a4bb15c746745e72c9baeb7c0eb4196
aniruddhasoni <anirudh.soni007@gmail.com>
2022-02-24 12:57:08 +0530

    Project page responsive

--- src/Routes/Project/Project.jsx
+++ src/Routes/Project/Project.jsx
@@ -1,30 +1,25 @@
 import React from "react";
-import "./Project.css";
 import avatar from "./../../assets/avatars/avatar_wink.svg";
 import ProjectGrid from "../../Components/Project/ProjectGrid";
-
 import json from "./../../data/project.json";
-import "./../../css/Responsive.css";
 
+import "./Project.css";
 import "./../../css/Responsive.css";
+import IntroGrid from "../../Components/Home/IntroGrid";
 
 export default function Project() {
   return (
     <section className="container">
-      <div className="project_grid_container">
-        <div className="grid intro">
-          <div className="intro_container">
-            <div className="intro">
-              Hi. this is <span className="name">Aniruddha Soni</span>,an
-              enthusiast web developer form India. Here are my some self and
-              freelance projects.
-            </div>
-            <a href="/about" className="button">
-              Know more...
-            </a>
-          </div>
-          <img src={avatar} alt="" className="avatar circle" />
+      <div className="project_grid_container ">
+        <div className="grid project_one">
+          <IntroGrid
+            avatar={avatar}
+            desc={
+              "an enthusiast web developer form India. Here are my some self and freelance projects."
+            }
+          />
         </div>
+
         {json.map((data) => (
           <div className="grid">
             <ProjectGrid data={data}></ProjectGrid>
