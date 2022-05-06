544fac1bbbec6ceb60650f2559140c3f63c756fc
Anirudh Soni <anirudh.soni007@gmail.com>
2022-05-06 15:20:14 +0530

    Update ResumeDownload.jsx

--- src/Components/Home/ResumeDownload.jsx
+++ src/Components/Home/ResumeDownload.jsx
@@ -4,7 +4,7 @@ import "./ResumeDownload.css";
 export default function ResumeDownload() {
   return (
     <div className="resume_container">
-      <a href="#">
+      <a href="https://drive.google.com/file/d/1JqM7Yob-YqzJFAhTIqjvirWEjfd3ynpd/view" download>
         <span className="material-icons resume_button">file_download</span>
       </a>
       <h3>Download Resume</h3>
