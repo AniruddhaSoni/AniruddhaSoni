81e2d20bcb325d6fb2aaac66d28c820debf224ad
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-12-24 01:02:28 +0530

    added threejs elemet

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Home from "../components/Home";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Aniruddh Soni | Portfolio</title>
+      </Head>
+      <Home/>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#7dd3fc",
+          secondary: "#93c5fd",
+          accent: "#38bdf8",
+          neutral: "#2A303C",
+          "base-100": "#191D24",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
