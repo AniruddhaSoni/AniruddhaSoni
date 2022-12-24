15c158a09212221f37c64b7a9d27dfbbbb1a70ff
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-12-25 03:25:50 +0530

    added about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,43 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-    <section id="about" className="h-screen">
-      About
+    <section
+      id="about"
+      className="min-h-screen flex w-10/12 m-auto items-center md:justify-between flex-col-reverse md:flex-row"
+    >
+      <div className="md:w-1/2">
+        <h1 className="text-4xl lg:text-8xl font-extrabold text-accent">
+          About
+        </h1>
+        <p className="text-lg md:text-xl lg:text-2xl mt-6">
+          Aniruddh Soni is my name. I&apos;m a talented developer from Rajasthan
+          who focuses on user interfaces, including rich experiences, design
+          systems, and command line tools. I like to create surprises and
+          delights for others. The internet as a medium for artistic expression
+          piques my attention. Numerous possibilities are made possible by
+          improvements in hardware and local web technologies as well as by the
+          internet&apos;s shared-by-default structure. There is still plenty to
+          learn about this area, which I have thoroughly loved visiting thus
+          far.
+        </p>
+
+        <p className="text-lg md:text-xl lg:text-2xl mt-6">
+          The internet as a medium for artistic expression piques my attention.
+          Numerous possibilities are made possible by improvements in hardware
+          and local web technologies as well as by the internet&apos;s
+          shared-by-default structure. There is still plenty to learn about this
+          area, which I have thoroughly loved visiting thus far.
+        </p>
+      </div>
+      <div className="w-[60vw] md:w-1/2 flex justify-center py-10 md:py-0">
+        <Image
+          src={"/images/computer.svg"}
+          alt=""
+          height={364.09}
+          width={624}
+        />
+      </div>
     </section>
   );
 }
--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,6 +1,6 @@
 export default function Testimonial() {
   return (
-    <div className="w-full font-black text-center h-[50vh] flex items-center justify-center text-secondary">
+    <div className="w-full font-black text-center py-4 flex items-center justify-center text-secondary">
       <span className="text-5xl">
         “Opportunities don&apos;t happen. You create them.”
       </span>
